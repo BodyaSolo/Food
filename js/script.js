3231c3670bcd8a1ddcf4fd11580908c7ed72be23
Bohdan Solonukha <bodyasolo@gmail.com>
2022-06-05 22:28:58 +0300

    create function hideTabContent

--- js/script.js
+++ js/script.js
@@ -4,4 +4,14 @@ window.addEventListener('DOMContentLoaded', () => {
     tabsContent = document.querySelectorAll('.tabcontent'),
     tabsParent = document.querySelector('.tabheader__items');
 
+  function hideTabContent() {
+    tabsContent.forEach(item => {
+      item.style.display = 'none';
+    });
+
+    tabs.forEach(item => { 
+      item.classList.remove('tabheader__item_active');
+    });
+  }
+  
 });
