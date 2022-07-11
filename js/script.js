6e65cf41563f9caea8621d7cd624db437f2cf5d4
Bohdan Solonukha <bodyasolo@gmail.com>
2022-07-11 17:45:50 +0300

    create function closeModal and event listener

--- js/script.js
+++ js/script.js
@@ -109,18 +109,27 @@ window.addEventListener('DOMContentLoaded', () => {
   
   modalTrigger.forEach(btn => {
     btn.addEventListener('click', () => {
-    // modal.classList.add('show');
-    // modal.classList.remove('hide');
     modal.classList.toggle('show');
     document.body.style.overflow = 'hidden';
   });
   });
-  
-  modalCloseBtn.addEventListener('click', () => {
-    // modal.classList.add('hide');
-    // modal.classList.remove('show');
+
+  function closeModal() {
     modal.classList.toggle('show');
     document.body.style.overflow = '';
+  }
+  
+  modalCloseBtn.addEventListener('click', closeModal);
+
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
   });
 
+  document.addEventListener('keydown', (e) => {
+    if (e.code === "Escape") {
+      closeModal();
+    }
+  });
 });
