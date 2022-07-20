b134c3d6d9599971ab541073cc816f0ffee10781
Bohdan Solonukha <bodyasolo@gmail.com>
2022-07-20 13:54:36 +0300

    add dot indicator to slider

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ window.addEventListener('DOMContentLoaded', () => {
       item.classList.remove('show', 'fade');
     });
 
-    tabs.forEach(item => { 
+    tabs.forEach(item => {
       item.classList.remove('tabheader__item_active');
     });
   }
@@ -53,7 +53,7 @@ window.addEventListener('DOMContentLoaded', () => {
       minutes = 0;
       seconds = 0;
     } else {
-        days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
         hours = Math.floor((t / (1000 * 60 * 60) % 24)),
         minutes = Math.floor((t / (1000 * 60) % 60)),
         seconds = Math.floor((t / 1000) % 60);
@@ -201,8 +201,8 @@ window.addEventListener('DOMContentLoaded', () => {
   };
 
   getResource('http://localhost:3000/menu')
-    .then(data => { 
-      data.forEach(({img, altimg, title, descr, price}) => {
+    .then(data => {
+      data.forEach(({ img, altimg, title, descr, price }) => {
         new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
       });
     });
@@ -217,7 +217,7 @@ window.addEventListener('DOMContentLoaded', () => {
     failure: 'Что-то пошло не так...'
   };
 
-  forms.forEach(item => { 
+  forms.forEach(item => {
     bindPostData(item);
   });
 
@@ -250,13 +250,13 @@ window.addEventListener('DOMContentLoaded', () => {
       const json = JSON.stringify(Object.fromEntries(formData.entries())); //formData to JSON
 
       postData('http://localhost:3000/requests', json)
-      .then(data => {
-        console.log(data);
-        showThanksModal(message.success);
-        statusMessage.remove();
-      })
-      .catch(() => showThanksModal(message.failure))
-      .finally(()=>form.reset());
+        .then(data => {
+          console.log(data);
+          showThanksModal(message.success);
+          statusMessage.remove();
+        })
+        .catch(() => showThanksModal(message.failure))
+        .finally(() => form.reset());
 
     });
     
@@ -277,7 +277,7 @@ window.addEventListener('DOMContentLoaded', () => {
         </div>
     `;
     document.querySelector('.modal').append(thanksModal);
-    setTimeout(() => { 
+    setTimeout(() => {
       thanksModal.remove();
       prevModalDialog.classList.add('show');
       prevModalDialog.classList.remove('hide');
@@ -292,6 +292,7 @@ window.addEventListener('DOMContentLoaded', () => {
   //Slides
 
   const slides = document.querySelectorAll('.offer__slide'),
+    slider = document.querySelector('.offer__slider'),
     prev = document.querySelector('.offer__slider-prev'),
     next = document.querySelector('.offer__slider-next'),
     total = document.querySelector('#total'),
@@ -321,6 +322,48 @@ window.addEventListener('DOMContentLoaded', () => {
     slide.style.width = width;
   });
 
+  slider.style.position = 'relative';
+  const indicators = document.createElement('ol'),
+    dots = [];
+  indicators.classList.add('carousel-indicators');
+  indicators.style.cssText = `
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 15;
+    display: flex;
+    justify-content: center;
+    margin-right: 15%;
+    margin-left: 15%;
+    list-style: none;
+  `;
+  slider.append(indicators);
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);
+    dot.style.cssText = `
+        box-sizing: content-box;
+        flex: 0 1 auto;
+        width: 30px;
+        height: 6px;
+        margin-right: 3px;
+        margin-left: 3px;
+        cursor: pointer;
+        background-color: #fff;
+        background-clip: padding-box;
+        border-top: 10px solid transparent;
+        border-bottom: 10px solid transparent;
+        opacity: .5;
+        transition: opacity .6s ease;
+      `;
+    if (i == 0) {
+      dot.style.opacity = 1;
+    }
+    indicators.append(dot);
+    dots.push(dot);
+  }
+
   next.addEventListener('click', () => {
     if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
       offset = 0;
@@ -333,7 +376,7 @@ window.addEventListener('DOMContentLoaded', () => {
     if (slideIndex == slides.length) {
       slideIndex = 1;
     } else {
-      slideIndex ++;
+      slideIndex++;
     }
 
     if (slides.length < 10) {
@@ -341,6 +384,9 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
       current.textContent = slideIndex;
     }
+
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[slideIndex - 1].style.opacity = '1';
   });
 
   prev.addEventListener('click', () => {
@@ -353,9 +399,9 @@ window.addEventListener('DOMContentLoaded', () => {
     slidesField.style.transform = `translateX(-${offset}px)`;
 
     if (slideIndex == 1) {
-      slideIndex = slideIndex.length;
+      slideIndex = slides.length;
     } else {
-      slideIndex --;
+      slideIndex--;
     }
 
     if (slides.length < 10) {
@@ -363,6 +409,29 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
       current.textContent = slideIndex;
     }
+
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[slideIndex - 1].style.opacity = 1;
+  });
+
+  dots.forEach(dot => {
+    dot.addEventListener('click', (e) => {
+      const slideTo = e.target.getAttribute('data-slide-to');
+
+      slideIndex = slideTo;
+      offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+      slidesField.style.transform = `translateX(-${offset}px)`;
+
+      if (slides.length < 10) {
+        current.textContent = `0${slideIndex}`;
+      } else {
+        current.textContent = slideIndex;
+      }
+
+      dots.forEach(dot => dot.style.opacity = '.5');
+      dots[slideIndex - 1].style.opacity = 1;
+    });
   });
 
 });
